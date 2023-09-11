4ccb4e10d89f2289bf0cd6d8e6d966809b63f60d
Frong <frongnoza@gmail.com>
2023-09-11 12:39:06 +0700

    + PauseMenu : Pause game panel
    can pause and go back to main menu

---
+++ PlaceholderURP/Assets/Scripts/PauseBotton/PauseBotton.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseBotton : MonoBehaviour
+{
+
+    public GameObject pauseMenu;
+
+    public static bool isPause;
+    void Start()
+    {
+        pauseMenu.SetActive(false);
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPause)
+            {
+                ResumeGame();
+
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    } 
+
+   public void PauseGame()
+        {
+            pauseMenu.SetActive(true);
+            Time.timeScale = 0f;
+            isPause = true;
+        }
+
+   public void ResumeGame()
+        {
+            pauseMenu.SetActive(false);
+            Time.timeScale = 1f;
+            isPause = false;
+        }
+
+    public void GoToMainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
